c53c46938be707dc4761b1182250d35f316aa8d6
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-07-04 15:35:41 +0700

    Update manualStructure.js

--- manual/manualStructure.js
+++ manual/manualStructure.js
@@ -1,1 +1,8 @@
-
+const createSubmenu = require('../utils/createSubmenu');
+const structureManual = createSubmenu('🏢 โครงสร้าง/ตำแหน่ง', [
+  {label: '🏛 โครงสร้างส่วนราชการ', uri: 'https://example.com/manuals/org-structure.pdf'},
+  {label: '🧾 จัดการ Template ตำแหน่ง', uri: 'https://example.com/manuals/position-template.pdf'},
+  {label: '📋 ขออนุมัติตำแหน่ง', uri: 'https://example.com/manuals/request-position.pdf'},
+  {label: '✏ ปรับปรุงตำแหน่ง', uri: 'https://example.com/manuals/edit-position.pdf'}
+]);
+module.exports = structureManual;
