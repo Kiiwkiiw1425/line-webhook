f67fbf084f8bceb19306170b4ee632257238915d
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-07-04 15:36:58 +0700

    Update manualBudget.js

--- manual/manualBudget.js
+++ manual/manualBudget.js
@@ -1,1 +1,5 @@
-
+const createSubmenu = require('../utils/createSubmenu');
+const budgetManual = createSubmenu('💰 บริหารวงเงิน', [
+  {label: '📊 วงเงินเจ้าหน้าที่ & ผู้บริหาร', uri: 'https://example.com/manuals/budget-management.pdf'}
+]);
+module.exports = budgetManual;
