ec3216489753b25759036ef37f51cdd66d683ef4
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-07-04 16:22:47 +0700

    Update manualUsageGeneral.js

--- manual/manualUsageGeneral.js
+++ manual/manualUsageGeneral.js
@@ -4,9 +4,9 @@ const createSubmenu = require('../utils/createSubmenu');
 const usageGeneralManual = createSubmenu('🔧 การใช้งานระบบทั่วไป', [
   { label: '📌 ลงทะเบียนใช้งาน DPIS6', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EX0QNBk-xCtAuJvgtZROkjwBdhmaMo5kGPz9KVggPe328Q?e=fGxFt5&web=1' },
   { label: '🔑 เปลี่ยนรหัสผ่าน', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EcOznyF3GF9Gqpt2cFRZS5IB8nHIaDuaX6hcrbeTaX9ssQ?e=K1Mhnd&web=1' },
-  { label: '📧 จำรหัสและอีเมลไม่ได้', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EcOznyF3GF9Gqpt2cFRZS5IB8nHIaDuaX6hcrbeTaX9ssQ?e=ZbN7S6&web=1' },
   { label: '🔑 เปลี่ยนรหัสผ่าน (จำรหัสได้)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EerK3yQLC-VJqOaw8ECO1PoBdXNkfrxpAbBglOijpn_KlA?e=8c9npa&download=1&web=1' },
   { label: '🔑 เปลี่ยนรหัสผ่าน (ลืมรหัส)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EerK3yQLC-VJqOaw8ECO1PoBdXNkfrxpAbBglOijpn_KlA?e=aELSQ5&web=1' },
+  { label: '📧 จำรหัสผ่านและอีเมลไม่ได้', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EcOznyF3GF9Gqpt2cFRZS5IB8nHIaDuaX6hcrbeTaX9ssQ?e=ZbN7S6&web=1' },
   { label: '📵 ไม่ได้ OTP/ปิดหน้าต่าง', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Eekfaq8zHF1Ku21OO4o-PEgB07LsEX6kwRwcAhtOdAQm-A?e=GmssiL&web=1' },
   { label: '👤 จัดการโปรไฟล์', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Eej0MVzCLWxIpJCWbVHPYtoBi2sfaMpl6nYUdHZOiptRoA?e=aBOQXh&web=1' },
   { label: '🎭 เปลี่ยนบทบาท', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EZzuAvmxY_ZChEDS3QTuBo0B1IQ31UFkW2sz7xnG33-Vfg?e=NHUCop&web=1' },
