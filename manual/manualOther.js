e8231a60d0f4a5a6cd132fcada918c9136a85bca
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-07-04 15:37:10 +0700

    Update manualOther.js

--- manual/manualOther.js
+++ manual/manualOther.js
@@ -1,1 +1,8 @@
-
+const createSubmenu = require('../utils/createSubmenu');
+const otherManual = createSubmenu('📖 อื่นๆ', [
+  {label: '📱 ติดตั้ง & ใช้งานแอปมือถือ', uri: 'https://example.com/manuals/mobile-install.pdf'},
+  {label: '🖼 ปรับขนาดภาพ (Paint 3D)', uri: 'https://example.com/manuals/resize-image.pdf'},
+  {label: '💬 วิธีใช้งาน LINE OA', uri: 'https://example.com/manuals/line-oa-guide.pdf'},
+  {label: '🔐 ตั้งค่าเข้าสู่ระบบด้วย ThaiD', uri: 'https://example.com/manuals/thaid-setting.pdf'}
+]);
+module.exports = otherManual;
