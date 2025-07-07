beff5d87d2e6662a851427c241d9a71ae98b1e63
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-07-07 14:30:37 +0700

    Update manualUpdate.js

--- manual/manualUpdate.js
+++ manual/manualUpdate.js
@@ -9,20 +9,20 @@ module.exports = {
       layout: 'vertical',
       spacing: 'lg',
       paddingAll: '20px',
-      backgroundColor: '#F9FBE7', //พื้นหลังสีเขียวอ่อน
+      backgroundColor: '#F9FBE7', // พื้นหลังสีเขียวอ่อน
       contents: [
         {
           type: 'text',
           text: '🛠 การอัปเดตระบบ',
           weight: 'bold',
           size: 'xl',
-          color: '#1B5E20', //เขียวเข้ม
+          color: '#1B5E20', // เขียวเข้ม
           align: 'center'
         },
         {
           type: 'button',
           style: 'primary',
-          color: '#558B2F', //เขียวเข้มสำหรับปุ่ม
+          color: '#558B2F', // เขียวเข้มสำหรับปุ่ม
           action: {
             type: 'uri',
             label: '📅 อัปเดตประจำวัน',
@@ -30,20 +30,14 @@ module.exports = {
           }
         },
         {
-          type: 'box',
-          layout: 'vertical',
-          paddingAll: '10px',
-          backgroundColor: '#EEEEEE', //สีเทาอ่อน
-          cornerRadius: 'md',
-          contents: [
-            {
-              type: 'text',
-              text: '📡 สถานะใช้งาน DPIS6 (ยังไม่เปิดใช้งาน)',
-              size: 'sm',
-              color: '#555555',
-              align: 'center'
-            }
-          ]
+          type: 'button',
+          style: 'primary',
+          color: '#558B2F', // เขียวเข้มสำหรับปุ่ม
+          action: {
+            type: 'uri',
+            label: '📡 สถานะใช้งาน DPIS6',
+            uri: 'https://sites.google.com/view/manual-dpis6/%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%83%E0%B8%8A%E0%B8%87%E0%B8%B2%E0%B8%99%E0%B8%A3%E0%B8%B0%E0%B8%9A%E0%B8%9A-dpis6'
+          }
         }
       ]
     }
