5b7d4a076c60b572d7965fcdee8dde815a0e510a
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-07-07 15:54:58 +0700

    Update flexMessages.js

--- flexMessages.js
+++ flexMessages.js
@@ -1,3 +1,18 @@
+// flexMessages.js
+const usageGeneralManual = require('./manual/manualUsageGeneral');
+const systemSettingManual = require('./manual/manualSystemSetting');
+const personnelManual = require('./manual/manualPersonnel');
+const permissionManual = require('./manual/manualPermission');
+const leaveManual = require('./manual/manualLeave');
+const structureManual = require('./manual/manualStructure');
+const commandManual = require('./manual/manualCommand');
+const reportManual = require('./manual/manualReport');
+const evaluationManual = require('./manual/manualEvaluation');
+const importExportManual = require('./manual/manualImportExport');
+const budgetManual = require('./manual/manualBudget');
+const otherManual = require('./manual/manualOther');
+const applicationManual = require('./manual/manualApplication');
+
 const mainMenu = {
   type: 'flex',
   altText: '📚 เมนูเลือกหมวดหมู่คู่มือ',
