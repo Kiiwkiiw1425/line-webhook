84f4e3ba7ea99823cb4c061e6ac6803ed1807aaa
Job <50175300+jumpjob0123@users.noreply.github.com>
2025-07-09 14:36:52 +0700

    Update manualBudget.js

--- manual/manualBudget.js
+++ manual/manualBudget.js
@@ -1,6 +1,5 @@
 const createSubmenu = require('../utils/createSubmenu');
 const budgetManual = createSubmenu('💰 บริหารวงเงิน', [
-  {label: '📊 บริหารวงเงินเจ้าหน้าที่/ผู้บริหาร', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EYdA0onMzxZBrPg6x0h87nsBzYrMzC7ZK_xWGoc1Ud4y1w?e=XVqjii'},
-  {label: '📊 ปรับเงินเดือน พรก. 1พ.ค.67และ68', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ETh5oRGtjChBiIlvvPJfZqUBUEWyB641WdkgphYgBUqyGQ?e=Asm2jE'}
+  {label: '📊 บริหารวงเงินเจ้าหน้าที่/ผู้บริหาร', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EYdA0onMzxZBrPg6x0h87nsBzYrMzC7ZK_xWGoc1Ud4y1w?e=XVqjii'}
 ]);
 module.exports = budgetManual;
