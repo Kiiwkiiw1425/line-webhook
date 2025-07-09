af5360f4e2b676601859495ed393983fd23da7eb
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-07-09 13:36:06 +0700

    Update manualStructure.js

--- manual/manualStructure.js
+++ manual/manualStructure.js
@@ -1,8 +1,8 @@
 const createSubmenu = require('../utils/createSubmenu');
 const structureManual = createSubmenu('🏢 โครงสร้าง/ตำแหน่ง', [
-  {label: '🏛 โครงสร้างส่วนราชการ', uri: 'https://example.com/manuals/org-structure.pdf'},
-  {label: '🧾 จัดการ Template ตำแหน่ง', uri: 'https://example.com/manuals/position-template.pdf'},
-  {label: '📋 ขออนุมัติตำแหน่ง', uri: 'https://example.com/manuals/request-position.pdf'},
-  {label: '✏ ปรับปรุงตำแหน่ง', uri: 'https://example.com/manuals/edit-position.pdf'}
+  {label: '🏛 โครงสร้างส่วนราชการ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ESNY8vgrVwFFgcbHqY42jv4BFkBYIRQoRmgoTPv4mkUHNA?e=meyBbU'},
+  {label: '🧾 จัดการ Template ตำแหน่ง', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ESDxNr5nWQhBo7iNY70SB_YBb3BIikbgDEX9YCArNQaMpw?e=1XSTmy'},
+  {label: '📋 ขอกำหนดตำแหน่ง', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ESxTve_QxfBMv6-WYo-VHmoBp8jjtsWmkQx2li__Li6HuA?e=Kv9Z5e'},
+  {label: '✏ ปรับปรุงตำแหน่ง', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ERgupgAHSftEoZZAP_Ody7YBtFeVWXj2ffmRlTEohLzmLg?e=qabGVS'}
 ]);
 module.exports = structureManual;
