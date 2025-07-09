6a2ae0e81e8656442f11c492b2bf0c4d2ca49a85
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-07-09 13:52:51 +0700

    Update manualCommand.js

--- manual/manualCommand.js
+++ manual/manualCommand.js
@@ -1,11 +1,14 @@
 const createSubmenu = require('../utils/createSubmenu');
-const commandManual = createSubmenu('📄 คำสั่ง', [
-  {label: '📥 บัญชีแนบท้าย (บรรจุ/โอน)', uri: 'https://example.com/manuals/command-recruit-transfer.pdf'},
-  {label: '⬆ เลื่อนระดับ', uri: 'https://example.com/manuals/promotion.pdf'},
-  {label: '🚪 ออกจากราชการ', uri: 'https://example.com/manuals/retirement.pdf'},
-  {label: '🔄 ย้าย/ช่วยราชการ', uri: 'https://example.com/manuals/transfer-secondment.pdf'},
-  {label: '⚖ รักษาการ/มอบหมาย', uri: 'https://example.com/manuals/acting-duty.pdf'},
-  {label: '💵 คำสั่งเงินเดือน ว9/2567', uri: 'https://example.com/manuals/salary-w9.pdf'},
-  {label: '📝 ยกเลิก/แก้ไขคำสั่ง', uri: 'https://example.com/manuals/revoke-edit-command.pdf'}
+const commandManual = createSubmenu('📄 คำสั่ง/บัญชีแนบท้าย', [
+  {label: '📥 บรรจุ/แต่งตั้ง/รับโอน)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Ece90tBH6XZBheUQuKRKv7gBu-1mqL9VnrXgMAceGoiAqQ?e=cPm4lX'},
+  {label: '⬆ เลื่อนระดับ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EZlzRhhGeydAo9arIXWo0-IBWgw5O2ypYIwQCod1qzN6SA?e=3Xdv5F'},
+  {label: '🚪 ให้โอน/พ้นจากราชการ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EWFIwsokAWZBpvPmMLQHHW4Bd37JAFgp6NPD5Fs31wqIYQ?e=LGj3TD'},
+  {label: '📝 เกษียณอายุบุคลากร', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EZ2GacnLqeVOtKNd0jqsIiQBlzj_U7ahtNW_zpOkB1rxkw?e=o2JDWY'},
+  {label: '🔄 ย้าย', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EUZkHqVQUwlIiWJukIpXhS0BxjhxfFqMTKsPZxj1lZdbVQ?e=C43qek'},
+  {label: '🔄 ช่วยราชการ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ER8Nc5LWpClAkVUXvW2drCQBJ9r3YSP7Qta3YGp2jNQQYg?e=sTu56B'},
+  {label: '⚖ รักษาการแทน', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Eeg9dRrUqAxMr9me8X_X2LkB8n1M0NHA24nxFmqMCJ2B2w?e=iJMh6Z'},
+  {label: '⚖ มอบหมายงาน', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EQ1NK04ZG0hLsswPIz7gThEBvBih1r7NkUQerQasWhHCtA?e=mRjWPe'},
+  {label: '💵 คำสั่งเงินเดือน ว9/2567', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EUpCeWNf6IRFsUiPKhnJm7kBGRax8FJF1I7t-aXxyjAhwg?e=m8KSaZ'},
+  {label: '📝 ยกเลิก/แก้ไขคำสั่ง', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EXpwo3HG1YBOshHmDi0VZOoBa58I3CeiOo4_pZbhYcoTCg?e=mEghxZ'}
 ]);
 module.exports = commandManual;
