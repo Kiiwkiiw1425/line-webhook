791848f266722b358fd3544afbfd3b6ea0b40a7b
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-07-09 11:56:39 +0700

    Update manualSystemSetting.js

--- manual/manualSystemSetting.js
+++ manual/manualSystemSetting.js
@@ -2,13 +2,13 @@
 const createSubmenu = require('../utils/createSubmenu');
 
 const systemSettingManual = createSubmenu('⚙ ตั้งค่าระบบและนโยบาย', [
-  { label: '📅 ตั้งค่าปีงบประมาณ', uri: 'https://example.com/manuals/fiscal-year-setting.pdf' },
-  { label: '🎛 ตั้งค่าการแสดงผล', uri: 'https://example.com/manuals/ui-config.pdf' },
-  { label: '📂 ตั้งค่าเมนูด้านข้าง', uri: 'https://example.com/manuals/sidebar-setting.pdf' },
-  { label: '🖥 ตรวจสอบระบบ', uri: 'https://example.com/manuals/server-check-update.pdf' },
-  { label: '📜 ปรับปรุงนโยบาย', uri: 'https://example.com/manuals/policy-update.pdf' },
-  { label: '🧹 ลบไฟล์ Back Up', uri: 'https://example.com/manuals/clean-backup.pdf' },
-  { label: '⚙ ตั้งค่าตัวแปรระบบ', uri: 'https://example.com/manuals/system-config.pdf' }
+  { label: '📅 ตั้งค่าปีงบประมาณ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EeSoPNxLh2NMtFshuOcjc4sBVs7Ss-XEHTj-35vqOpseGA?e=DgvI05' },
+  { label: '🎛 ตั้งค่าการแสดงผล', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EbfdRWo8ZAhLswTJaGVc3DAByLf3slS4Jo6jhfU3nb6ioA?e=qEV0E5' },
+  { label: '📂 ตั้งค่าเมนูด้านข้าง', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EX3c2jyd86NNhZCZv1NkjrUBETX5q1TES_pykD262K650g?e=19QKV2' },
+  { label: '🖥 ตรวจสอบระบบ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EY3TZQuCwb1Lu_dTIyh7s30B8IQZT3N_FnkKn5tawOtn5g?e=muiY1M' },
+  { label: '📜 ปรับปรุงนโยบาย', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Ed4r5bp3Ap1Fr8LK8KGErOMBgVPlBaXUk_AQaHEErg6u_g?e=CjXos3' },
+  { label: '🧹 ลบไฟล์ Back Up', uri: 'hhttps://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Eb_nifk3pItFti6lPXZ_54kBhoeATNtBtt47_DSMURd1lA?e=FGrHG3' },
+  { label: '⚙ ตั้งค่าตัวแปรระบบ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ES02TRgtHwVOuVN2B9oemDABsCSn3ZS2BVOyLBHK4lPwDQ?e=HvDcKU' }
 ]);
 
 module.exports = systemSettingManual;
