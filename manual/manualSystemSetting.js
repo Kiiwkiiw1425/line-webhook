cf0e06e0fae79e21a7365013328bb53216d5097f
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-07-09 14:20:06 +0700

    Update manualSystemSetting.js

--- manual/manualSystemSetting.js
+++ manual/manualSystemSetting.js
@@ -7,7 +7,7 @@ const systemSettingManual = createSubmenu('⚙ ตั้งค่าระบบ
   { label: '📂 ตั้งค่าเมนูด้านข้าง', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EX3c2jyd86NNhZCZv1NkjrUBETX5q1TES_pykD262K650g?e=19QKV2' },
   { label: '🖥 ตรวจสอบระบบ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EY3TZQuCwb1Lu_dTIyh7s30B8IQZT3N_FnkKn5tawOtn5g?e=muiY1M' },
   { label: '📜 ปรับปรุงนโยบาย', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Ed4r5bp3Ap1Fr8LK8KGErOMBgVPlBaXUk_AQaHEErg6u_g?e=CjXos3' },
-  { label: '🧹 ลบไฟล์ Back Up', uri: 'hhttps://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Eb_nifk3pItFti6lPXZ_54kBhoeATNtBtt47_DSMURd1lA?e=FGrHG3' },
+  { label: '🧹 ลบไฟล์ Back Up', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Eb_nifk3pItFti6lPXZ_54kBhoeATNtBtt47_DSMURd1lA?e=FGrHG3' },
   { label: '⚙ ตั้งค่าตัวแปรระบบ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ES02TRgtHwVOuVN2B9oemDABsCSn3ZS2BVOyLBHK4lPwDQ?e=HvDcKU' }
 ]);
 
