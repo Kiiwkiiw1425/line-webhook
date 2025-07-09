be5c69fcb513446ba0578c7d901d62e25cc5e018
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-07-09 13:56:22 +0700

    Update manualReport.js

--- manual/manualReport.js
+++ manual/manualReport.js
@@ -1,6 +1,6 @@
 const createSubmenu = require('../utils/createSubmenu');
 const reportManual = createSubmenu('📊 รายงาน', [
-  {label: '📈 รายงานสถิติการรับ/สูญเสีย', uri: 'https://example.com/manuals/personnel-stat-report.pdf'},
-  {label: '📋 รายงานแบบตาราง', uri: 'https://example.com/manuals/tabular-report.pdf'}
+  {label: '📈 บรรจุ/รับโอน/สูญเสีย', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EbakEvTyRr9JvblgrPUCmwgBa68HSxscmO1UHFjTlVzWWg?e=EW4LwD'},
+  {label: '📋 รายงานแบบตาราง', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EUpc7e6qSXREsXMs0TwNX94BJr4Q1UmQhW0wvS7S13y7gA?e=ldsKK4'}
 ]);
 module.exports = reportManual;
