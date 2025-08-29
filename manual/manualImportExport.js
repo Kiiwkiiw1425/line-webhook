e52836f862921e90e12d1317bdce1709a1b75896
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-08-29 10:15:19 +0700

    Update manualImportExport.js

--- manual/manualImportExport.js
+++ manual/manualImportExport.js
@@ -3,6 +3,8 @@ const importExportManual = createSubmenu('📥 นำเข้า/ส่งอ�
   {label: '💵 นำเข้าข้อมูลเลื่อนเงินเดือน', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ETdFlrSHQFNAsYfkx4KMDNIBRFcID_rJjll0KaNQU_k8BQ?e=kJVW1b'},
   {label: '📄 นำเข้าข้อมูลภาษี', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EY1qA0a3UJlGkgoxRYUhrXIBr5lp4axHy6m9xsY7gTS3dQ?e=VJUMie'},
   {label: '🧾 นำเข้าสลิปเงินเดือน', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EWj5opp9BuNCv_nPot3Hor4B4Jn-D8oeLnau0B17ue5tUg?e=59au8l'},
-  {label: '📎 นำเข้าไฟล์แนบท้ายคำสั่ง', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EdAdnLJJ2lxFlBjy80oE47gBSVP-5lMBdbX94MKfM-6f9Q?e=IU9Z5y'}
+  {label: '📎 นำเข้าไฟล์แนบท้ายคำสั่ง', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EdAdnLJJ2lxFlBjy80oE47gBSVP-5lMBdbX94MKfM-6f9Q?e=IU9Z5y'},
+  {label: '📤 ส่งออกข้อมูลข้าราชการ (GMIS)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EYKpH5HtrRdBoTWSaWekZ78BdwxbQz-XKBSEOvBfzZ_4AA?e=WdWAdu'},
+  {label: '📤 ส่งออกข้อมูลพนักงานราชการ (GEIS)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ERhqIRc1GI9Jscu6WfqMl68BmeKhkRBgnhUc4oq8QGjCWA?e=9Yt20w'}
 ]);
 module.exports = importExportManual;
