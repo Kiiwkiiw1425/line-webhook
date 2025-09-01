6efe3224725b123217e03f587983d362d3b7106e
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-09-01 10:46:21 +0700

    Update manualOther.js

--- manual/manualOther.js
+++ manual/manualOther.js
@@ -1,7 +1,6 @@
 const createSubmenu = require('../utils/createSubmenu');
 const otherManual = createSubmenu('📖 อื่นๆ', [
   {label: '📱 ติดตั้ง & ใช้งานแอปมือถือ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EbNMDQz1C05MoM24oDCobZcBzKQuQKQw78GPEv8LatjM1Q?e=d2XCA7'},
-  {label: '💬 วิธีใช้งาน LINE OA', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EU0OnqEJ_kxBhEV8nC8Ap3UBnNDf0HIbSPZhAnEuTRdgeQ?e=7njXc6'},
-  {label: '🔐 ตั้งค่าเข้าสู่ระบบด้วย ThaiD', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EY_ntN9-15JMhnjhMFNeTR0B4aiaEz4t3ruhXXfAHEHOUA?e=RGTEhe'}
+  {label: '💬 วิธีใช้งาน LINE OA', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EU0OnqEJ_kxBhEV8nC8Ap3UBnNDf0HIbSPZhAnEuTRdgeQ?e=7njXc6'}
 ]);
 module.exports = otherManual;
