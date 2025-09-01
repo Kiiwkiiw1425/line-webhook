de720355ca069bba21bd0f453dd49ec96c415ad8
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2025-09-01 11:40:22 +0700

    Update manualSystemSetting.js

--- manual/manualSystemSetting.js
+++ manual/manualSystemSetting.js
@@ -9,10 +9,10 @@ const systemSettingManual = createSubmenu('⚙ ตั้งค่าระบบ
   { label: '📜 ปรับปรุงนโยบาย', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Ed4r5bp3Ap1Fr8LK8KGErOMBgVPlBaXUk_AQaHEErg6u_g?e=CjXos3' },
   { label: '🧹 ลบไฟล์ Back Up', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Eb_nifk3pItFti6lPXZ_54kBhoeATNtBtt47_DSMURd1lA?e=FGrHG3' },
   { label: '⚙ ตั้งค่าตัวแปรระบบ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ES02TRgtHwVOuVN2B9oemDABsCSn3ZS2BVOyLBHK4lPwDQ?e=HvDcKU' },
-  { label: 'ตั้งค่า(Email/ID Card)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ETwgTtbDg4RPrFi17YEDaNMB-yv0px0wBfYaa20WixK0lg?e=mvHDgk' },
-  { label: 'ตั้งค่า(LDAP)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Eade9U6fhGlJpMkLyu1ssqIBwG38O0vBGkQ4UVhKJHzFFw?e=l0yOcG' },
-  { label: 'ตั้งค่า(Smart Card)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Edp_saUvNqBHpjpXr1HK0SoBHipNY3IZ6u5JgYV2aSNIDw?e=donLby' },
-  { label: 'ตั้งค่า(Username และ Password)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EQroPfCb7LJNtRw7Y-f3EZoBzFAI72Lnfuaob5TkyEaiMw?e=IF3gzT' },
-  { label: 'ตั้งค่า(ThaID)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EY_ntN9-15JMhnjhMFNeTR0B4aiaEz4t3ruhXXfAHEHOUA?e=RGTEhe'}
+  { label: '🔐 ตั้งค่า(Email/ID Card)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ETwgTtbDg4RPrFi17YEDaNMB-yv0px0wBfYaa20WixK0lg?e=mvHDgk' },
+  { label: '🔐 ตั้งค่า(LDAP)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Eade9U6fhGlJpMkLyu1ssqIBwG38O0vBGkQ4UVhKJHzFFw?e=l0yOcG' },
+  { label: '🔐 ตั้งค่า(Smart Card)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Edp_saUvNqBHpjpXr1HK0SoBHipNY3IZ6u5JgYV2aSNIDw?e=donLby' },
+  { label: '🔐 ตั้งค่า(Username และ Password)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EQroPfCb7LJNtRw7Y-f3EZoBzFAI72Lnfuaob5TkyEaiMw?e=IF3gzT' },
+  { label: '🔐 ตั้งค่า(ThaID)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EY_ntN9-15JMhnjhMFNeTR0B4aiaEz4t3ruhXXfAHEHOUA?e=RGTEhe'}
 ]);
 module.exports = systemSettingManual;
