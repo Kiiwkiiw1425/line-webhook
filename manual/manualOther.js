c211e65f7d390958d962199b15cedee5bebba0de
Job <50175300+jumpjob0123@users.noreply.github.com>
2025-12-04 16:08:14 +0700

    Update manualOther.js

--- manual/manualOther.js
+++ manual/manualOther.js
@@ -1,6 +1,7 @@
 const createSubmenu = require('../utils/createSubmenu');
 const otherManual = createSubmenu('📖 อื่นๆ', [
   {label: '📱 ติดตั้ง & ใช้งานแอปมือถือ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EbNMDQz1C05MoM24oDCobZcBzKQuQKQw78GPEv8LatjM1Q?e=d2XCA7'},
-  {label: '💬 วิธีใช้งาน LINE OA', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EZ5ONaEVXFVKjkDOdVaVMA8BHpDr29K8-h3c1OCxc50WOw?e=skvtZ4'}
+  {label: '💬 วิธีใช้งาน LINE OA', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EZ5ONaEVXFVKjkDOdVaVMA8BHpDr29K8-h3c1OCxc50WOw?e=skvtZ4'},
+  {label: '💬 วิธีแนบลายเซ็นต์/รูปโปรไฟล์', uri:'https://ocscthailand.sharepoint.com/:i:/s/ictUnit-DPIS/IQBt2cu5PqmHQoKpVzrT7EMqAdltt9qYlQMB3yy-IBncxcc?e=P9VhCw'}
 ]);
 module.exports = otherManual;
