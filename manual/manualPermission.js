d1a3ce8ac27b215f1b45e4d230d06f02e903ee4c
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2026-03-16 15:28:13 +0700

    Update manualPermission.js

--- manual/manualPermission.js
+++ manual/manualPermission.js
@@ -6,6 +6,6 @@ const permissionManual = createSubmenu('🔐 สิทธิการใช้�
   {label: '📝 จัดการข้อมูลลงทะเบียน', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EbU3dIJmjaRAm9q9-zO3OpwB6xXV31OQkvbIvMX7Q3iUJw?e=j1LU14'},
   {label: '👁 ตั้งค่าสิทธิการมองเห็น', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EacaKpNjTh1NuNjF3rYSrv0BFYb8WoRnkZgtfU02eGljog?e=SWTS6q'},
   {label: '➕ เพิ่มกลุ่มผู้ใช้งาน', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EX_d1XF7IHRLiCqt3nF8x1sBI0wPNnxHWxLtoPCYIr9-Iw?e=mjXQz1'},
-  {label: '✍ ตั้งค่าผู้ลงนาม', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EWee543ftyxLl5sXcjjWCiwBblLVMVKReAiwZLMDG4cF_A?e=tTSJRn'}
+  {label: '➕ การเพิ่มแอดมินสูงสุด', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/IQAnsxYmleM5SYkcBx1qa4UTAbh70wmhDCjhRhEGKHLXYss?e=Y12YfF'}
 ]);
 module.exports = permissionManual;
