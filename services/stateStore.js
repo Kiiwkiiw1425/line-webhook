fd1c50ea174c35ccda06d3123361030ad1ace4c5
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2026-04-03 14:46:32 +0700

    Update stateStore.js

--- services/stateStore.js
+++ services/stateStore.js
@@ -1,19 +1,34 @@
+// services/stateStore.js
 const store = {};
 
-function getState(id){ return store[id] || {}; }
-function setState(id, s){ store[id] = {...getState(id), ...s}; }
-function clearConversation(id){ delete store[id]; }
-
-function shouldNotify(s={}){
-  const now = Date.now();
-  return now - (s.notifiedAt||0) > 3*60*1000;
+function getState(id) {
+  return store[id] || {};
 }
 
-function touch(id){
-  setState(id,{
+function setState(id, newState) {
+  store[id] = { ...getState(id), ...newState };
+}
+
+function clearConversation(id) {
+  delete store[id];
+}
+
+function shouldNotify(state = {}) {
+  const now = Date.now();
+  return now - (state.notifiedAt || 0) > 3 * 60 * 1000;
+}
+
+function touch(id) {
+  setState(id, {
     lastActivity: Date.now(),
     promptedAfterInactive: false
   });
 }
 
-module.exports = { getState,setState,clearConversation,shouldNotify,touch };
+module.exports = {
+  getState,
+  setState,
+  clearConversation,
+  shouldNotify,
+  touch
+};
