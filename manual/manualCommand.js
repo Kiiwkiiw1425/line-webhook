c7823d00b2541cf0e0242fd3547f807c18f3408e
Kiiwkiiw1425 <sirikiat.kiiw@gmail.com>
2026-04-10 13:37:27 +0700

    Update manualCommand.js

--- manual/manualCommand.js
+++ manual/manualCommand.js
@@ -1,16 +1,56 @@
-const createSubmenu = require('../utils/createSubmenu');
-const commandManual = createSubmenu('📄 คำสั่ง/บัญชีแนบท้าย', [
-  {label: '📥 บรรจุ/แต่งตั้ง/รับโอน)', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Ece90tBH6XZBheUQuKRKv7gBu-1mqL9VnrXgMAceGoiAqQ?e=cPm4lX'},
-  {label: '📝 การบรรจุข้าราชการ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EXOayl9HHalEte5rd_-RY0kBB4tMn9JXImfngsf-vVmpHw?e=V1lxwl'},
-  {label: '⬆ เลื่อนระดับ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EZlzRhhGeydAo9arIXWo0-IBWgw5O2ypYIwQCod1qzN6SA?e=3Xdv5F'},
-  {label: '🚪 ให้โอน/พ้นจากราชการ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EWFIwsokAWZBpvPmMLQHHW4Bd37JAFgp6NPD5Fs31wqIYQ?e=LGj3TD'},
-  {label: '📊 ปรับเงินเดือนพรก.1พ.ค.67และ68', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ETh5oRGtjChBiIlvvPJfZqUBUEWyB641WdkgphYgBUqyGQ?e=Asm2jE'},
-  {label: '📝 เกษียณอายุราชการ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EZ2GacnLqeVOtKNd0jqsIiQBlzj_U7ahtNW_zpOkB1rxkw?e=iuKw9y'},
-  {label: '🔄 ย้าย', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EUZkHqVQUwlIiWJukIpXhS0BxjhxfFqMTKsPZxj1lZdbVQ?e=C43qek'},
-  {label: '🔄 ช่วยราชการ', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ER8Nc5LWpClAkVUXvW2drCQBJ9r3YSP7Qta3YGp2jNQQYg?e=sTu56B'},
-  {label: '⚖ รักษาการแทน', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Eeg9dRrUqAxMr9me8X_X2LkB8n1M0NHA24nxFmqMCJ2B2w?e=iJMh6Z'},
-  {label: '⚖ มอบหมายงาน', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EQ1NK04ZG0hLsswPIz7gThEBvBih1r7NkUQerQasWhHCtA?e=mRjWPe'},
-  {label: '💵 ปรับเงินเดือนตามคุณวุฒิ ว9/2567', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EUpCeWNf6IRFsUiPKhnJm7kBGRax8FJF1I7t-aXxyjAhwg?e=m8KSaZ'},
-  {label: '📝 ยกเลิก/แก้ไขคำสั่ง', uri: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EXpwo3HG1YBOshHmDi0VZOoBa58I3CeiOo4_pZbhYcoTCg?e=mEghxZ'}
-]);
-module.exports = commandManual;
+// manual/manualCommand.js
+module.exports = {
+  orderAttachment: {
+    title: '📄 คำสั่ง / บัญชีแนบท้าย',
+    items: [
+      {
+        label: '📥 บรรจุ / แต่งตั้ง / รับโอน',
+        url: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Ece90tBH6XZBheUQuKRKv7gBu-1mqL9VnrXgMAceGoiAqQ'
+      },
+      {
+        label: '📝 การบรรจุข้าราชการ',
+        url: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EXOayl9HHalEte5rd_-RY0kBB4tMn9JXImfngsf-vVmpHw'
+      },
+      {
+        label: '⬆ เลื่อนระดับ',
+        url: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EZlzRhhGeydAo9arIXWo0-IBWgw5O2ypYIwQCod1qzN6SA'
+      },
+      {
+        label: '🚪 ให้โอน / พ้นจากราชการ',
+        url: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EWFIwsokAWZBpvPmMLQHHW4Bd37JAFgp6NPD5Fs31wqIYQ'
+      },
+      {
+        label: '📊 ปรับเงินเดือน พรก. 1 พ.ค. 67–68',
+        url: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ETh5oRGtjChBiIlvvPJfZqUBUEWyB641WdkgphYgBUqyGQ'
+      },
+      {
+        label: '📝 เกษียณอายุราชการ',
+        url: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EZ2GacnLqeVOtKNd0jqsIiQBlzj_U7ahtNW_zpOkB1rxkw'
+      },
+      {
+        label: '🔄 ย้าย',
+        url: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EUZkHqVQUwlIiWJukIpXhS0BxjhxfFqMTKsPZxj1lZdbVQ'
+      },
+      {
+        label: '🔄 ช่วยราชการ',
+        url: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/ER8Nc5LWpClAkVUXvW2drCQBJ9r3YSP7Qta3YGp2jNQQYg'
+      },
+      {
+        label: '⚖ รักษาการแทน / รักษาการในตำแหน่ง',
+        url: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/Eeg9dRrUqAxMr9me8X_X2LkB8n1M0NHA24nxFmqMCJ2B2w'
+      },
+      {
+        label: '⚖ มอบหมายงาน / ปฏิบัติราชการแทน',
+        url: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EQ1NK04ZG0hLsswPIz7gThEBvBih1r7NkUQerQasWhHCtA'
+      },
+      {
+        label: '💵 ปรับเงินเดือนตามคุณวุฒิ ว9/2567',
+        url: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EUpCeWNf6IRFsUiPKhnJm7kBGRax8FJF1I7t-aXxyjAhwg'
+      },
+      {
+        label: '📝 ยกเลิก / แก้ไขคำสั่ง',
+        url: 'https://ocscthailand.sharepoint.com/:b:/s/ictUnit-DPIS/EXpwo3HG1YBOshHmDi0VZOoBa58I3CeiOo4_pZbhYcoTCg'
+      }
+    ]
+  }
+};
